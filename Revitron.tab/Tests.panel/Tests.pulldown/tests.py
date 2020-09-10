f108ffbc770ca49ba0cbfc11090e62b5f60048c9
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-09-10 10:50:29 +0200

    Delete tests.py

--- Revitron.tab/Tests.panel/Tests.pulldown/tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from StringIO import StringIO
-
-def run(testClass):
-	stream = StringIO()
-	runner = unittest.TextTestRunner(stream=stream, verbosity=2)
-	result = runner.run(unittest.makeSuite(testClass))
-	stream.seek(0)
-	print (stream.read())
