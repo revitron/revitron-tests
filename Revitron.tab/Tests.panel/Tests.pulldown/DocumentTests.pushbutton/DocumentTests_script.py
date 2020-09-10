5f1ccc63ef1b5b736625f5249ccf1c4830d2d7a4
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-09-10 17:20:31 +0200

    Create DocumentTests_script.py

---
+++ Revitron.tab/Tests.panel/Tests.pulldown/DocumentTests.pushbutton/DocumentTests_script.py
@@ -0,0 +1,27 @@
+import revitron
+import unittest
+import os 
+import sys
+from revitron import _
+
+dn = os.path.dirname
+sys.path.append(dn(dn(dn(dn(dn(__file__))))))
+
+import revitrontests
+
+__context__ = 'zero-doc'
+
+class DocumentTests(revitrontests.RevitronTestCase):
+	
+	def testIsFamily(self):
+		self.assertFalse(revitron.Document().isFamily())
+	
+	def testConfigStorage(self):
+		config = revitron.DocumentConfigStorage()
+		config.set('test.1', {'key': 'value'})
+		config.set('test.2', 'string')
+		raw = revitron._(revitron.DOC.ProjectInformation).get(config.storageName)
+		self.assertEquals(raw, '{"test.1": {"key": "value"}, "test.2": "string"}')
+		self.assertEquals(config.get('test.2'), 'string')
+	
+revitrontests.run(DocumentTests)
