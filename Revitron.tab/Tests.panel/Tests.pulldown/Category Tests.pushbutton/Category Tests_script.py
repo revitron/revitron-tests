dbb670f24537c2b6bf2870b246e83c4e7c132260
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-09-10 10:50:46 +0200

    Update Category Tests_script.py

--- Revitron.tab/Tests.panel/Tests.pulldown/Category Tests.pushbutton/Category Tests_script.py
+++ Revitron.tab/Tests.panel/Tests.pulldown/Category Tests.pushbutton/Category Tests_script.py
@@ -1,16 +1,24 @@
-import revitron, unittest, os, sys
+import revitron
+import unittest
+import os 
+import sys
 
-path = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(path)
+dn = os.path.dirname
+sys.path.append(dn(dn(dn(dn(dn(__file__))))))
 
-import tests
+import test
+
+__context__ = 'zero-doc'
 
 class CatergoryTests(unittest.TestCase):
     
+    def setUp(self):
+        f = test.Fixture()
+        
     def testGet(self):
         self.assertEqual(revitron.Category('Walls').get().Name, 'Walls')
         
     def testGetBic(self):
         self.assertEqual(revitron.Category('Walls').getBic(), revitron.DB.BuiltInCategory.OST_Walls)
       
-tests.run(CatergoryTests)
+test.run(CatergoryTests)
