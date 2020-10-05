f7d2da2d7a9677629f0bad2fab82b993fa40bd7d
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-05 18:56:33 +0200

    Update RoomTests_script.py

--- Revitron.tab/Tests.panel/Tests.pulldown/RoomTests.pushbutton/RoomTests_script.py
+++ Revitron.tab/Tests.panel/Tests.pulldown/RoomTests.pushbutton/RoomTests_script.py
@@ -32,7 +32,7 @@ class RoomTests(revitrontests.RevitronTestCase):
 		]
 		for i in range(0, 4):
 			self.assertTrue(boundaryPoints[i].IsAlmostEqualTo(testPoints[i]))
-			
+
 	def testRoomInsetPoints(self):
 		xyz = revitron.DB.XYZ
 		room = self.fixture.createRoom()
@@ -46,24 +46,24 @@ class RoomTests(revitrontests.RevitronTestCase):
 		for i in range(0, 4):
 			self.assertTrue(boundaryPoints[i].IsAlmostEqualTo(testPoints[i]))
 
-	def testRoomTopLeft(self):
+	def testRoomPointTopLeft(self):
 		room = self.fixture.createRoomComplex()
-		topLeft = _(room).getTopLeftPoint(0.5)
-		self.assertTrue(topLeft.IsAlmostEqualTo(revitron.DB.XYZ(2.5,11.5,0)))
+		point = _(room).getPointTopLeft(0.5)
+		self.assertTrue(point.IsAlmostEqualTo(revitron.DB.XYZ(2.5,11.5,0)))
 
-	def testRoomTopRight(self):
+	def testRoomPointTopRight(self):
 		room = self.fixture.createRoomComplex()
-		topLeft = _(room).getTopRightPoint(0.5)
-		self.assertTrue(topLeft.IsAlmostEqualTo(revitron.DB.XYZ(11.5,11.5,0)))
+		point = _(room).getPointTopRight(0.5)
+		self.assertTrue(point.IsAlmostEqualTo(revitron.DB.XYZ(11.5,11.5,0)))
   
-	def testRoomBottomLeft(self):
+	def testRoomPointBottomLeft(self):
 		room = self.fixture.createRoomComplex()
-		topLeft = _(room).getBottomLeftPoint(0.5)
-		self.assertTrue(topLeft.IsAlmostEqualTo(revitron.DB.XYZ(0.5,2.5,0)))
+		point = _(room).getPointBottomLeft(0.5)
+		self.assertTrue(point.IsAlmostEqualTo(revitron.DB.XYZ(0.5,2.5,0)))
   
-	def testRoomBottomRight(self):
+	def testRoomPointBottomRight(self):
 		room = self.fixture.createRoomComplex()
-		topLeft = _(room).getBottomRightPoint(0.5)
-		self.assertTrue(topLeft.IsAlmostEqualTo(revitron.DB.XYZ(13.5,4.5,0)))
+		point = _(room).getPointBottomRight(0.5)
+		self.assertTrue(point.IsAlmostEqualTo(revitron.DB.XYZ(13.5,4.5,0)))
 
 revitrontests.run(RoomTests)
