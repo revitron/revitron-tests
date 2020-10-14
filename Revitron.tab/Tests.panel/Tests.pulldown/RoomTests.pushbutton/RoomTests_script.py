b068d138a34c3501309c685fac397cc1c69af3d9
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-10-14 19:00:48 +0200

    Update RoomTests_script.py

--- Revitron.tab/Tests.panel/Tests.pulldown/RoomTests.pushbutton/RoomTests_script.py
+++ Revitron.tab/Tests.panel/Tests.pulldown/RoomTests.pushbutton/RoomTests_script.py
@@ -7,14 +7,7 @@ __context__ = 'zero-doc'
 
 class RoomTests(revitrontests.RevitronTestCase):
 	
-	def testRoomTag(self):
-		room = self.fixture.createRoom()
-		t = revitron.Transaction()
-		tag = _(room).tagCenter()
-		t.commit()
-		self.assertEquals(tag.Id.IntegerValue, _(room).getTags()[0].Id.IntegerValue)
-	
-	def testRoomCenter(self):
+ 	def testRoomCenter(self):
 		room = self.fixture.createRoom()
 		center = _(room).getBboxCenter()
 		self.assertEquals(center.X, 5.0)
