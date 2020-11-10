c45897bb13929004fd727ee28206812567a60882
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-11-10 18:17:10 +0100

    Update BoundingBoxTests_script.py

--- Revitron.tab/Tests.panel/Tests.pulldown/BoundingBoxTests.pushbutton/BoundingBoxTests_script.py
+++ Revitron.tab/Tests.panel/Tests.pulldown/BoundingBoxTests.pushbutton/BoundingBoxTests_script.py
@@ -14,4 +14,10 @@ class BoundingBoxTests(revitrontests.RevitronTestCase):
 		self.assertTrue(_(wall1).getBbox().containsXY(_(wall1).getBbox()))
 		self.assertFalse(_(wall2).getBbox().containsXY(_(wall1).getBbox()))
 
+	def testGetCenterPoint(self):
+		room = self.fixture.createRoom()
+		center = _(room).getBbox().getCenterPoint()
+		self.assertEquals(center.X, 5.0)
+		self.assertEquals(center.Y, 5.0)
+
 revitrontests.run(BoundingBoxTests)
