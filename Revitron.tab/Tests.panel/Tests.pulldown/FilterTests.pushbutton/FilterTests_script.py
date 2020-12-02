f5fe25684a7b70c84a9e623d5a1b1a35e714807a
Marc Anton Dahmen <revitron@marcdahmen.de>
2020-12-02 21:23:09 +0100

    Update FilterTests_script.py

--- Revitron.tab/Tests.panel/Tests.pulldown/FilterTests.pushbutton/FilterTests_script.py
+++ Revitron.tab/Tests.panel/Tests.pulldown/FilterTests.pushbutton/FilterTests_script.py
@@ -8,7 +8,7 @@ __context__ = 'zero-doc'
 class FilterTests(revitrontests.RevitronTestCase):
 	
 	def testStringFilters(self):
-     
+	 
 		w1 = self.fixture.createWall([0, 10], [10, 10])
 		w2 = self.fixture.createWall([0, 20], [10, 20])
 		w3 = self.fixture.createWall([0, 30], [10, 30])
@@ -26,21 +26,29 @@ class FilterTests(revitrontests.RevitronTestCase):
 		toStr = revitrontests.idsToStr		
 		
 		self.assertEquals(toStr([w1.Id, w2.Id]),
-            toStr(f().byStringContains('test', 'awesome').noTypes().getElementIds()))
+			toStr(f().byStringContains('test', 'awesome').noTypes().getElementIds()))
 
 		self.assertEquals(toStr([w4.Id, w5.Id]),
-            toStr(f().byCategory('Walls').byStringContains('test', 'wall', True).noTypes().getElementIds()))
+			toStr(f().byCategory('Walls').byStringContains('test', 'wall', True).noTypes().getElementIds()))
  
 		self.assertEquals(toStr([w3.Id]), 
-            toStr(f().byStringBeginsWith('test', 'and one').noTypes().getElementIds()))
+			toStr(f().byStringBeginsWith('test', 'and one').noTypes().getElementIds()))
 
 		self.assertEquals(toStr([w1.Id, w2.Id, w3.Id]), 
-            toStr(f().byStringEndsWith('test', 'wall').noTypes().getElementIds()))
+			toStr(f().byStringEndsWith('test', 'wall').noTypes().getElementIds()))
 
 		self.assertEquals(toStr([w3.Id]), 
-            toStr(f().byStringEquals('test', 'and one more wall').noTypes().getElementIds()))
+			toStr(f().byStringEquals('test', 'and one more wall').noTypes().getElementIds()))
 		
 		self.assertEquals(toStr([w4.Id]), 
-            toStr(f().byStringEquals('Comments', 'test comment').noTypes().getElementIds()))
+			toStr(f().byStringEquals('Comments', 'test comment').noTypes().getElementIds()))
+
+	def testRegexFilter(self):
+		family = self.fixture.createGenericModelFamily()
+		genModel = self.fixture.createGenericModelInstance(family, revitron.DB.XYZ(0,0,0))
+		f = revitron.Filter
+		toStr = revitrontests.idsToStr
+		self.assertEquals(toStr([genModel.Id]),
+			toStr(f().noTypes().byRegex('Family and Type', '\-genericModel\-Revit').getElementIds()))
 
 revitrontests.run(FilterTests)
